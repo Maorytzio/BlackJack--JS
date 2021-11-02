517f9fa6dc743ec53a786bdaaea684c95046aa45
Maor <marmaor@gmail.com>
2021-11-02 15:26:48 +0200

    init Crash Course

---
+++ home.js
@@ -0,0 +1,30 @@
+// var age = prompt("Whats your age?");
+
+//document.getElementById("someText").innerHTML = age;
+
+// numbers
+
+// var num = 5.7;
+// console.log((num/10).toFixed(2));
+// console.log((num/10).toFixed(2) == 0.57);
+
+console.clear();
+
+var min = parseFloat("5").toFixed(2);
+var max = parseFloat("10").toFixed(2);
+var value = parseFloat("7").toFixed(2);
+
+console.log(min, max, value);
+
+console.log(value > min); // OK.
+console.log(parseFloat(value) < parseFloat(max)); // ---- false ??????
+
+var userName = prompt("Please enter your name");
+
+function hello(userName) {
+  return document.getElementById("someText").innerHTML = "hello there, " + userName;
+}
+
+hello(userName);
+
+
