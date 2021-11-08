bfdce205fa9e5c661599eec51f206a8ec3382a25
Maor <marmaor@gmail.com>
2021-11-08 15:48:48 +0200

    finished challenge2: Cat Generator (-:

--- proj1/static/js/script.js
+++ proj1/static/js/script.js
@@ -1,3 +1,4 @@
+//Age In Days:
 function ageInDays() {
   var birthYear = prompt("What year were you born?");
   var days = (2021 - birthYear) * 365;
@@ -6,4 +7,20 @@ function ageInDays() {
   h1.setAttribute('id', 'ageInDays');
   h1.appendChild(textAnswer);
   document.getElementById('flex-box-result').appendChild(h1);
+}
+
+
+function reset() {
+    document.getElementById('ageInDays').remove();
+
+}
+
+
+//Cat Generator:
+function generateCat() {
+    var image = document.createElement('img');
+    image.classList.add('magic-cat');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&timestamp";
+    div.appendChild(image);
 }
